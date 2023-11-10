2a3406d14bf831e22024a69938733cc43e298b26
Abhishek Kodad <43044936+abhishek-kodad@users.noreply.github.com>
2023-11-10 20:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (9.10%) - LeetSync

---
+++ Arrays/54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> ans;
+    void addBoundary(int firstRow, int lastRow, int firstCol, int lastCol, vector<vector<int>>& a){
+        for(int j=firstCol; j<lastCol; j++) ans.push_back(a[firstRow][j]);
+        for(int i=firstRow; i<lastRow; i++) ans.push_back(a[i][lastCol]);
+    //    if (firstCol<lastCol)
+            for(int j=lastCol; j>firstCol; j--) ans.push_back(a[lastRow][j]);
+    //    if (firstRow<lastRow)
+            for(int i=lastRow; i>firstRow; i--) ans.push_back(a[i][firstCol]);
+    }
+    vector<int> spiralOrder(vector<vector<int>>& a) {
+        int n=a.size();
+        int m=a[0].size();
+        int firstRow=0, lastRow=n-1, firstCol=0, lastCol=m-1;
+        while(firstRow<lastRow && firstCol<lastCol){
+            addBoundary(firstRow++, lastRow--, firstCol++, lastCol--, a);
+        }
+        cout<<firstRow<<"\n"<<lastRow<<"\n"<<firstCol<<"\n"<<lastCol<<"\n";
+        if (firstRow>lastRow || firstCol>lastCol){
+            cout<<"case 1";
+            return ans;
+        }         
+        if ( firstRow==lastRow && firstCol<=lastCol){
+            for(int j=firstCol; j<=lastCol; j++) ans.push_back(a[firstRow][j]);
+            cout<<"case 2";
+        } 
+            
+        else if ( firstCol==lastCol && firstRow<=lastRow ){
+            for(int i=firstRow; i<=lastRow; i++) ans.push_back(a[i][lastCol]);
+            cout<<"case 3";
+        }
+           
+        return ans;
+    }
+};
