5e2babde2fc439c753fe3abe727c39482e8ed69a
Abhishek Kodad <43044936+abhishek-kodad@users.noreply.github.com>
2024-05-09 22:32:45 +0530

    Time: 15 ms (76.91%) | Memory: 27.8 MB (39.68%) - LeetSync

--- Arrays/109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ Arrays/109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -1,41 +1,24 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* build(vector<int>& ans, int left, int right){
-        if(left>right){return NULL;}
-
-        int mid= (left+right)/2;
-        TreeNode* root = new TreeNode(ans[mid]);
-        root->left = build(ans,left,mid-1);
-        root->right = build(ans, mid+1,right);
-        return root;
-    }
-    TreeNode* sortedListToBST(ListNode* head) {
+    vector<int> getArray(ListNode* head){
         vector<int> ans;
-        while(head!=NULL){
+        while(head){
             ans.push_back(head->val);
             head=head->next;
         }
-        return build(ans,0,ans.size()-1);
+        return ans;
+    }
+    TreeNode* createBinarySearchTree(TreeNode* root,vector<int>& arr,int s,int e){
+        if(s>e) return NULL;
+        int mid=s+(e-s)/2;
+        root=new TreeNode(arr[mid]);
+        root->left=createBinarySearchTree(root->left,arr,s,mid-1);
+        root->right=createBinarySearchTree(root->right,arr,mid+1,e);
+        return root;
+    }
+    TreeNode* sortedListToBST(ListNode* head) {
+        TreeNode* root=NULL;
+        vector<int> arr=getArray(head);
+        return createBinarySearchTree(root,arr,0,arr.size()-1);
     }
 };
